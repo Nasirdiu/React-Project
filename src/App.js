9b50f40f5005d906b714cd20a505686c0e8265f1
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-19 20:18:56 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div >
-      <h1 className="text-5xl">hello</h1>
+    <div>
+      <Navbar/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='text-white'>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Hero;
